5cbaddab69ffc4900658e5f0cc65f0e7c48d104a
lepratham <prathamshah87@gmail.com>
2025-11-30 02:34:12 +0530

    Added circleshape.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from logger import log_state
 
 pygame.init()
 
+clock = pygame.time.Clock()
+
+dt = 0
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -17,6 +21,7 @@ def main():
         log_state()
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
